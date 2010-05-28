9f3f5f569dd5beaa64debabeecbf00f026376168
Nick Stielau <nick.stielau@gmail.com>
2010-05-27 22:04:16 -0700

    Removing unused 'type' property from constructor.

--- lib/autograph/graph_series.rb
+++ lib/autograph/graph_series.rb
@@ -4,8 +4,7 @@ class GraphSeries
   attr :label, true
   attr :path, true
 
-  def initialize(t, xs, ys, l, p=nil)
-    @type = t
+  def initialize(xs, ys, l, p=nil)
     @x_values = xs.map{|x| x.to_f}
     @y_values = ys.map{|x| x.to_f}
     @label = l
